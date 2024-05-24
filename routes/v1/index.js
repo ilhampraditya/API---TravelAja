089cc0f1bf18dd5411fd15c23b1741380b72682f
Ilham Praditya <130032787+ilhampraditya@users.noreply.github.com>
2024-05-24 19:36:03 +0700

    add api-docs (#7)

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../v1/user.route")
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yaml");
+const fs = require("fs");
+const path = require("path");
+
+const swagger_path = path.resolve(__dirname, "../../docs/openapi.yaml");
+const file = fs.readFileSync(swagger_path, "utf-8");
+
+// API Docs
+const swaggerDocument = YAML.parse(file);
+router.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 router.use("/api/v1", User);
 
