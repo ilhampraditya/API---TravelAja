b57f75af36d32ce149c7a0d221cedbaa73deb351
Arif Pradana <119426536+arifprdn@users.noreply.github.com>
2024-05-30 04:08:53 +0700

    fix and add some endpoint (#29)

--- routes/v1/airlines.routes.js
+++ routes/v1/airlines.routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { image } = require("../../libs/multer");
 const {
   getAllAirlines,
   createAirline,
 } = require("../../controllers/airlines.controllers");
 
 router.get("/airlines", getAllAirlines);
-router.post("/airlines", createAirline);
+router.post("/airlines", image.single("file"), createAirline);
 
 module.exports = router;
