655ae49bac367571fc2ff2db15959df00b10a0a2
arifprdn <arifpradana315@gmail.com>
2024-06-07 21:31:46 +0700

    tes midtrans

--- prisma/migrations/20240607093658_/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "bookings" ADD COLUMN     "snap_redirect_url" TEXT,
-ADD COLUMN     "snap_token" TEXT;
-
--- AlterTable
-ALTER TABLE "payments" ALTER COLUMN "status" SET DEFAULT 'PENDING_PAYMENT';
