60cf91cb50c49e42a137968bf5926241aadb47cb
Arif Pradana <119426536+arifprdn@users.noreply.github.com>
2024-06-13 17:23:46 +0700

    Final (#79)
    * fix endpoint search flight
    
    * fix documentation
    
    * fix seat by class id
    
    * fix google oauth
    
    * fix google oauth2
    
    * fix google oauth3
    
    * done

--- routes/v1/flights.routes.js
+++ routes/v1/flights.routes.js
@@ -6,11 +6,12 @@ const {
   createFlight,
   searchFlight,
 } = require("../../controllers/flights.controller");
+const { restrict } = require("../../middlewares/auth.middleware");
 
 
 router.get("/flights", getAllFlights);
 router.get("/flights/:id", getFlightById);
-router.post("/flights", createFlight);
+router.post("/flights", restrict, createFlight);
 router.get("/search", searchFlight)
 
 module.exports = router;
