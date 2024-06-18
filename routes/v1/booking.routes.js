51ee95d87d8cf950165cc7872c61a519ec9a591c
Arif Pradana <119426536+arifprdn@users.noreply.github.com>
2024-06-18 22:16:32 +0700

    Arifdeploy6 (#88)
    * fix endpoint search flight
    
    * fix documentation
    
    * fix seat by class id
    
    * fix google oauth
    
    * fix google oauth2
    
    * fix google oauth3
    
    * done
    
    * done
    
    * done
    
    * done
    
    * fix something
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug

--- routes/v1/booking.routes.js
+++ routes/v1/booking.routes.js
@@ -10,7 +10,7 @@ const {
 const router = express.Router();
 
 router.get("/booking", getAllBooking);
-router.get("/booking/:id", restrict, getBookingById)
+router.get("/booking/id/:id", restrict, getBookingById)
 router.get("/booking/history", restrict, bookingHistory);
 router.post("/booking", restrict, createBooking);
 router.post("/booking/process", restrict, BookingProcess);
