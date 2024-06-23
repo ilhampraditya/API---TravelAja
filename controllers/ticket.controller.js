2c13aec68f708635586b79b8cf004a1e47d15eae
Arif Pradana <119426536+arifprdn@users.noreply.github.com>
2024-06-24 00:01:57 +0700

    Arifdeploy8 (#95)
    * fix endpoint search flight
    
    * fix documentation
    
    * fix seat by class id
    
    * fix google oauth
    
    * fix google oauth2
    
    * fix google oauth3
    
    * done
    
    * done
    
    * done
    
    * done
    
    * fix something
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * fixing bug
    
    * add cronjobs

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -72,6 +72,14 @@ module.exports = {
                 passengers: passengers
             };
 
+            if (!tickets) {
+                return res.status(404).json({
+                    status: true,
+                    message: "Data tiket tidak ditemukan!",
+                    data: tickets,
+                });
+            }
+
             return res.status(200).json({
                 status: true,
                 message: "Data tiket berhasil diambil",
