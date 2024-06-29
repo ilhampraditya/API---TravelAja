ceb5cd6872ce58720641acef48c1fa286dfbef4b
Ilham Praditya <130032787+ilhampraditya@users.noreply.github.com>
2024-06-29 21:11:07 +0700

    Delete libs directory

--- libs/formatdate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    formatdate: (timestamp) => {
-        let date = new Date(timestamp);
-        let options = { day: "numeric", month: "long", year: "numeric" };
-        let formattedDate = new Intl.DateTimeFormat("id-ID", options).format(date);
-        return formattedDate;
-    },
-};
--- libs/otpGenerator.js
+++
@@ -1,8 +0,0 @@
-const { randomInt } = require('crypto');
-
-// Function to generate OTP
-const generateOTP = () => {
-  return randomInt(100000, 999999);
-};
-
-module.exports = { generateOTP };
